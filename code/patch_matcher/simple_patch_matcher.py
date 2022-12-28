03507bea16bbea9b75bb3d9965dd7aac9ed724c6
Tirnanic Filip (uic52421) <filip.tirnanic@continental-corporation.com>
2022-12-28 14:47:09 +0100

    DOC: Documented simple patch matcher script

--- code/patch_matcher/simple_patch_matcher.py
+++ code/patch_matcher/simple_patch_matcher.py
@@ -6,7 +6,7 @@ from patch_matcher.patch_matcher_utility import first_and_second_smallest
 
 class SimplePatchMatcher(PatchMatcher):
 
-    def __init__(self, template_img, pw, ph, verbose=0):
+    def __init__(self, template_img, pw: int, ph: int, verbose: int = 0):
         # init parent constructor
         super().__init__(verbose)
         # preprocess image
@@ -19,29 +19,45 @@ class SimplePatchMatcher(PatchMatcher):
             # extract key points from template
             self.template_key_points = self.extract_key_points(self.template)
             # extract template features
-            self.template_key_points, self.template_features = self.extract_features(self.template_key_points,
-                                                                                     self.template)
+            self.template_features = self.extract_features(self.template_key_points, self.template)
             # nomalize features
             self.template_features = self.normalize_features(self.template_features)
         else:
             self.template_key_points = []
             self.template_features = []
 
-    # override abstract method
-    # every point in image is key point
-    # returns x,y key points location
-    def extract_key_points(self, image):
+    def extract_key_points(self, image: np.array) -> np.array:
+        """
+        Extracts key points from image (override abstract method).
+        Each pixel in image is key point.
+
+        :param image: Input image
+        :return: Key points
+        """
+
         key_points_list = []
+        # iterate through each pixel
         for y in range(math.floor(self.ph / 2), image.shape[0] - math.floor(self.ph / 2) + 1):
             for x in range(math.floor(self.pw / 2), image.shape[1] - math.floor(self.pw / 2) + 1):
+                # ad position of pixel as key point
                 key_points_list.append((x, y))
+
         key_points = np.array(key_points_list)
+
         return key_points
 
-    # override abstract method
-    # return features for each key point
-    def extract_features(self, key_points, image):
+    def extract_features(self, key_points: np.array, image: np.array) -> np.array:
+        """
+        Extracts feature around key points from image (override abstract method).
+        Features are raveled patch pixel values.
+
+        :param key_points: Array of tuple representing key points
+        :param image: Input image
+        :return: Features for each key point
+        """
+
         features = []
+        # iterate over key points
         for i in np.arange(0, key_points.shape[0]):
             # get key point location
             x, y = key_points[i]
@@ -54,11 +70,19 @@ class SimplePatchMatcher(PatchMatcher):
             features.append(feature)
 
         features = np.array(features)
-        return key_points, features
 
-    # simple matcher just uses one feature for patch so it will be just one match
-    # outputs list of meatched features
-    def match_features(self, patch_features, template_features):
+        return features
+
+    def match_features(self, patch_features: np.array, template_features: np.array) -> np.array:
+        """
+        Match features extracted from patch to template features (override abstract method).
+        Finds the nearest feature from template features.
+
+        :param patch_features: Patch features
+        :param template_features: Template features
+        :return: Array of tuple with indices pairs of matched features
+        """
+
         match = []
         # match features
         for i in np.arange(0, patch_features.shape[0]):
@@ -67,18 +91,28 @@ class SimplePatchMatcher(PatchMatcher):
             # calculate dist from ith path_feature to each template_feature
             distance = np.sqrt(np.sum((template_features - patch_feature) ** 2, axis=1))
             m1, i1, m2, i2 = first_and_second_smallest(distance)
-
+            # append matched indices
             match.append((i1, i))
 
         return match
 
-    # override abstract method
-    # output top let postion of template where the patch match
-    def find_corresponding_location_of_patch(self, patch_key_points, match):
+    def find_corresponding_location_of_patch(self, patch_key_points: np.array, match: np.array):
+        """
+        Finds the location of top left corner of the patch in template image (override abstract method).
+        Take just first matched template point as center of match.
+
+        :param patch_key_points: Patch key points array
+        :param match: Match array
+        :return: Top left position in template image and match array
+        """
+
+        # get first match indices
         i_kp_template, i_kp_patch = match[0]
 
+        # get template key point
         template_center_match = self.template_key_points[i_kp_template]
 
+        # calculate patch top left location in template image
         x_left_top = template_center_match[0] - math.floor(self.pw / 2)
         y_left_top = template_center_match[1] - math.floor(self.ph / 2)
         return x_left_top, y_left_top, match
