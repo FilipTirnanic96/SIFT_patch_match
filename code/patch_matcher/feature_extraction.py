017ef9eb7edfb546ac02e9e0037319a3d408cb09
Tirnanic Filip (uic52421) <filip.tirnanic@continental-corporation.com>
2022-12-28 14:15:55 +0100

    DOC: Documented feature extraction script

--- code/patch_matcher/feature_extraction.py
+++ code/patch_matcher/feature_extraction.py
@@ -2,13 +2,26 @@ import numpy as np
 
 
 def compute_gradient_histogram(num_bins, gradient_magnitudes, gradient_angles):
+    """
+    Computes gradient histogram from gradient magnitudes and angles
+
+    :param num_bins: Number of bins in gradient histogram
+    :param gradient_magnitudes: Gradient magnitudes
+    :param gradient_angles: Gradient angles
+    :return:Gradient histogram
+    """
+
+    # init bin angle array
     angle_step = 2 * np.pi / num_bins
     angles = np.arange(0, 2 * np.pi + angle_step, angle_step)
-
+    # digitize gradient angles
     indices = np.digitize(gradient_angles.ravel(), bins=angles)
     indices -= 1
+    # ravel gradient magnitudes
     gradient_magnitudes_ravel = gradient_magnitudes.ravel()
+    # init histogram
     histogram = np.zeros(num_bins)
+    # fill gradient histogram
     for i in range(0, indices.shape[0]):
         histogram[indices[i]] += gradient_magnitudes_ravel[i]
 
@@ -16,22 +29,31 @@ def compute_gradient_histogram(num_bins, gradient_magnitudes, gradient_angles):
 
 
 def weight_gradient_histogram(histogram, coefficient):
+    """
+    Weight gradient histogram bins according to distance from maximum value
+
+    :param histogram: Gradient histogram
+    :param coefficient: Weighting coefficient
+    :return:Weighted gradient histogram
+    """
+
     if coefficient >= 1 or coefficient < 0:
         return histogram
 
     #  find max index
     max_index = np.argmax(histogram)
+
+    # weight right side
     multiply = coefficient
     i = max_index + 1
-
     while i < len(histogram):
         histogram[i] *= multiply
         multiply *= coefficient
         i += 1
-
+    # weight left side
     i = max_index - 1
     multiply = coefficient
-    while i > 0:
+    while i > -1:
         histogram[i] *= multiply
         multiply *= coefficient
         i -= 1
